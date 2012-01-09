1551a939f2da16b9676439f148b1381b4e2f8c97
Markus Korpinen <markus.korpinen@gmail.com>
2012-01-09 20:33:03 +0200

    tiedostoja lisätty ja koodia muokattu

---
+++ global.mod
@@ -0,0 +1,46 @@
+GFORTRAN module version '4' created from tsp_genetic.f95 on Mon Jan  9 20:12:41 2012
+MD5:aee10d2dcb81ec53d94776b517258e55 -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 '__convert_r4_r8' '(intrinsic)' '__convert_r4_r8' 1 ((PROCEDURE
+UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 FUNCTION ELEMENTAL PURE)
+(REAL 8 0 0 REAL ()) 0 0 () () 2 () () () 0 0)
+3 'city' 'global' 'city' 1 ((DERIVED UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0) (UNKNOWN 0 0 0 UNKNOWN ()) 0 0 () () 0 ((4 'name' (
+CHARACTER 1 0 0 CHARACTER ((CONSTANT (INTEGER 4 0 0 INTEGER ()) 0 '20')))
+() (UNKNOWN-FL UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN UNKNOWN 0 0)
+UNKNOWN-ACCESS ()) (5 'lat' (REAL 4 0 0 REAL ()) () (UNKNOWN-FL
+UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN UNKNOWN 0 0) UNKNOWN-ACCESS ()) (6
+'lon' (REAL 4 0 0 REAL ()) () (UNKNOWN-FL UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0) UNKNOWN-ACCESS ())) PUBLIC (() () () ()) () 0 0
+61326187)
+7 'deg_to_rad' 'global' 'deg_to_rad' 1 ((PARAMETER UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 REAL ()) 0 0 () (
+CONSTANT (REAL 8 0 0 REAL ()) 0 '0.477d1aaaaaaaac@-1') () 0 () () () 0 0)
+8 'double' 'global' 'double' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 INTEGER ()) 0 0 () (CONSTANT (
+INTEGER 4 0 0 INTEGER ()) 0 '8') () 0 () () () 0 0)
+9 'global' 'global' 'global' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0) (UNKNOWN 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0 0)
+10 'pi' 'global' 'pi' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (REAL 8 0 0 REAL ()) 0 0 () (CONSTANT (REAL 8 0 0
+REAL ()) 0 '0.3243f6c0000000@1') () 0 () () () 0 0)
+11 'selected_real_kind' '(intrinsic)' 'selected_real_kind' 1 ((
+PROCEDURE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN UNKNOWN 0 0 FUNCTION) (
+UNKNOWN 0 0 0 UNKNOWN ()) 0 0 () () 11 () () () 0 0)
+)
+
+('__convert_r4_r8' 0 2 'city' 0 3 'deg_to_rad' 0 7 'double' 0 8 'global'
+0 9 'pi' 0 10 'selected_real_kind' 0 11)
---
+++ tsp_genetic.c
@@ -0,0 +1,108 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+
+typedef struct City{
+	char name[80];
+	double latitude;
+	double longitude;
+	} City;
+	
+int count_lines(FILE *fptr);
+void print_city(City *city);
+void read_cities(FILE *fptr, City* cities[], int count);
+double to_radians(double deg);
+double distance(City* from, City* to);
+double path_distance(City** cities, int count);
+
+int main(void) {
+	int n;
+	FILE* fptr = fopen("p03.txt", "r");
+	assert(fptr != NULL);
+	
+	
+	n =	count_lines(fptr);
+	printf("\nNumber of cities: %i\n",n);
+	
+	City* cities[n];
+	read_cities(fptr, cities, n);
+	
+	print_city(cities[0]);
+	
+	printf("\nDistance between Helsinki and Espoo: %f\n", distance(cities[1],cities[4]));
+	
+	printf("\nDistance through default order of cities: %f\n", path_distance(cities,n));
+	
+	/*lueTiedosto(&x, &y, &size);
+
+	// Tulostetaan taulukon arvot 	
+	printf("%lf %lf\n", x[i], y[i]);
+	for(i = 0; i < size; i++) {
+  	       printf("%lf %lf\n", x[i], y[i]);
+        }
+	*/
+return 0;
+}
+
+void print_city(City *city){
+	
+	printf("\n%s (%lf %lf)\n",city->name,city->latitude,city->longitude);
+	
+}
+
+int count_lines(FILE *fptr){
+	int n=0;
+	char str[81];
+	rewind(fptr);
+		while(fgets(str, 80, fptr) != NULL) {
+		if((str[0] != '#') && (str[0] != '\n')) {
+			n++;
+		}
+	}
+	return n;
+}
+
+void read_cities(FILE *fptr, City* cities[], int count){
+	rewind(fptr);
+	int n=0;
+	char str[81];
+	
+	while(fgets(str, 80, fptr) != NULL) {
+		if((str[0] != '#') && (str[0] != '\n')) {
+			cities[n] = malloc(sizeof(City));
+			sscanf(str, "%s %lf %lf", cities[n]->name, &cities[n]->latitude, &cities[n]->longitude);
+			n++;
+		}
+	}
+	return;	
+}
+
+double to_radians(double deg) {
+	return deg*(3.14159/180);
+}
+
+double distance(City* from, City* to) {
+	double f_lat_rad = to_radians(from->latitude);
+	double t_lat_rad = to_radians(to->latitude);
+	double f_lon_rad = to_radians(from->longitude);
+	double t_lon_rad = to_radians(to->longitude);
+
+	double diff_lat = fabs(t_lat_rad - f_lat_rad);
+	double diff_lon = fabs(t_lon_rad - f_lon_rad);
+
+	double a = sin(diff_lat/2)*sin(diff_lat/2) + (cos(f_lat_rad)*cos(t_lat_rad)*sin(diff_lon/2)*sin(diff_lon/2));
+
+	double c = 2*atan2(sqrt(a), sqrt(1-a));
+
+	return c*6370;
+}
+
+double path_distance(City** cities, int count) {
+	double dist = 0;
+	for(int i = 0; i < count-1; i++) {
+		dist += distance(cities[i], cities[i+1]);
+	}
+	dist += distance(cities[count-1], cities[0]);
+	return dist;
+}
